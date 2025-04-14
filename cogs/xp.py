4a82e7e0449bb49ec28590fe94d41c8a14028386
mattarm555 <83235730+mattarm555@users.noreply.github.com>
2025-04-14 15:58:14 -0400

    Update xp.py

--- cogs/xp.py
+++ cogs/xp.py
@@ -7,14 +7,14 @@ import random
 
 # --- Console Colors ---
 RESET = "\033[0m"
-BOLD = "\033[1m"
-DIM = "\033[2m"
-ITALIC = "\033[3m"
-UNDERLINE = "\033[4m"
-BLINK = "\033[5m"
-REVERSE = "\033[7m"
-HIDDEN = "\033[8m"
+BLACK = "\033[30m"
 RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+MAGENTA = "\033[35m"
+CYAN = "\033[36m"
+WHITE = "\033[37m"
 
 # --- XP Settings ---
 XP_FILE = "xp_data.json"
