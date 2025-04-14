abc06cee3c3b9688766a2d652a28ca54a0ec5a45
mattarm555 <83235730+mattarm555@users.noreply.github.com>
2025-04-14 16:08:27 -0400

    Update events.py

--- cogs/events.py
+++ cogs/events.py
@@ -5,13 +5,18 @@ from datetime import datetime
 import pytz
 
 # --- Color Codes ---
-GREEN = "\033[92m"
-YELLOW = "\033[93m"
-CYAN = "\033[96m"
 RESET = "\033[0m"
+BLACK = "\033[30m"
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+MAGENTA = "\033[35m"
+CYAN = "\033[36m"
+WHITE = "\033[37m"
 
 def debug_command(name, user, **kwargs):
-    print(f"{GREEN}[COMMAND] /{name}{RESET} triggered by {YELLOW}{user.display_name}{RESET}")
+    print(f"{RED}[COMMAND] /{name}{RESET} triggered by {YELLOW}{user.display_name}{RESET}")
     if kwargs:
         print(f"{CYAN}Input:{RESET}")
         for key, value in kwargs.items():
