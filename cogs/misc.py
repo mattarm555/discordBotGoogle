1c7f5b2d9372e8a7f412e257ea29e56df2d3e6d9
mattarm555 <83235730+mattarm555@users.noreply.github.com>
2025-04-14 15:59:33 -0400

    Update misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -7,6 +7,16 @@ import pytz
 from utils.debug import debug_command
 import asyncio
 
+RESET = "\033[0m"
+BLACK = "\033[30m"
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+MAGENTA = "\033[35m"
+CYAN = "\033[36m"
+WHITE = "\033[37m"
+
 
 class HelpPaginator(ui.View):
     def __init__(self, pages):
